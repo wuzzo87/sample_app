f6f06eb687a7d6fd5964efcf700e3e3b1b416c40
Tim Tune <wuzzo87@yahoo.com>
2012-07-20 15:19:18 +1000

    Fixed delete user functionality with button_to

--- config/application.rb
+++ config/application.rb
@@ -61,6 +61,8 @@ module SampleApp
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
 
   end
 end
